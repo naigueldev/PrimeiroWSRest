d09e3afb7cb8cb1400ef9732ce977469b52cfcac
naigueldev <naiguelsantosdeveloper@gmail.com>
2017-03-07 20:55:21 -0300

    metodo put

--- src/com/upf/graducao/OlaMundo.java
+++ src/com/upf/graducao/OlaMundo.java
@@ -7,10 +7,16 @@
  */
 package com.upf.graducao;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
 
 @Path("/OlaMundo")
 public class OlaMundo {
@@ -27,6 +33,18 @@ public class OlaMundo {
 	public String testeDeNomeEscroto(){
 		return "Só uma prova...[POST]";
 	}
-	
+	//EXERCÍCIO 1
+	// Faça com que esse recurso responda a uma requisição
+	// put, retornando a data atual (Dica, classe date)
+	@PUT
+	@Produces("text/plain")
+	public String dataAtual(){     
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(dtf.format(now));
+        return dtf.format(now).toString();
+		
+	}
+
 	
 }
